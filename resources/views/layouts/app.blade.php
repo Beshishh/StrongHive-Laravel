e1ddc542fe6b1ff5c148e05dec453c11e9403f6a
Beshishh <70581429+Beshishh@users.noreply.github.com>
2023-05-07 16:09:47 +0300

    Admin navbar update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -127,14 +127,6 @@
           <div class="collapse navbar-collapse justify-content-end" id="navigation">
 
             <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link" href="#pablo">
-                  <i class="now-ui-icons media-2_sound-wave"></i>
-                  <p>
-                    <span class="d-lg-none d-md-block">Stats</span>
-                  </p>
-                </a>
-              </li>
               <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown"
                   aria-haspopup="true" aria-expanded="false">
@@ -144,18 +136,10 @@
                   </p>
                 </a>
                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
+                <a class="dropdown-item" href="{{ url('/') }}">Back to main page</a>
                   <a class="dropdown-item" href="#">Edit profile</a>
                   <a class="dropdown-item" href="{{ url('/logout')}}">Log out</a>
-                  <a class="dropdown-item" href="#">Something else here</a>
                 </div>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#pablo">
-                  <i class="now-ui-icons users_single-02"></i>
-                  <p>
-                    <span class="d-lg-none d-md-block">Account</span>
-                  </p>
-                </a>
               </li>
             </ul>
           </div>
