ed0a1759c8362185ee7cbfce824ef1f588d151a9
wisayy <davidguzhavin@gmail.com>
2023-05-08 03:11:21 +0300

    Login, Register remade

--- resources/views/start.blade.php
+++ resources/views/start.blade.php
@@ -2,48 +2,54 @@
 
 @section('content')
 <hr>
-
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-3">
-            <section class="login_content text-center">
-                @if(session()->has('error'))
-                <div class="alert alert-danger">
-                    {{ session()->get('error') }}
-                </div>
-                @endif
-                <p class="login-box-smg">Sign in to start your session</p>
-                <form action="{{url('login')}}" method="POST">
-                    @csrf
-                    <div class="login-form-header">
-                        <h1>StrongHive</h1>
-                    </div>
-                    <div class="form-group">
-                        <input type="text" class="form-control" name="email" placeholder="Email" required="" />
-                    </div>
-                    <div class="form-group">
-                        <input type="password" class="form-control" name="password" placeholder="Password" required="" />
-                    </div>
-                    <div class="form-group">
-                        <button type="submit" class="btn btn-default submit" name="login">Log in</button>
-                    </div>
-                    <div class="clearfix"></div>
-                    <div class="separator">
-                        <p class="change_link">New to site?
-                            <a href="{{url('register')}}" class="to_register"> Create Account </a>
-                        </p>
-                        <p class="change_link">
-                            <a href="{{url('/')}}"> Back to main page </a>
-                        </p>
-                        <div class="clearfix"></div>
-                        <br />
-                        <div>
-                            <p>©2023 IVKHK</p>
+<main>
+<body style="background-color:#242423;">
+    <div class="login-register">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-3">
+                    <section class="login_content text-center">
+                        @if(session()->has('error'))
+                        <div class="alert alert-danger">
+                            {{ session()->get('error') }}
                         </div>
-                    </div>
-                </form>
-            </section>
+                        @endif
+                        <p class="login-box-smg">Sign in to start your session</p>
+                        <form action="{{url('login')}}" method="POST">
+                            @csrf
+                            <div class="login-form-header">
+                                <h1>StrongHive</h1>
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control" name="email" placeholder="Email" required="" />
+                            </div>
+                            <div class="form-group">
+                                <input type="password" class="form-control" name="password" placeholder="Password" required="" />
+                            </div>
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-default submit" name="login">Log in</button>
+                            </div>
+                            <div class="clearfix"></div>
+                            <div class="separator">
+                                <p class="change_link">New to site?
+                                    <a href="{{url('register')}}" class="to_register"> Create Account </a>
+                                </p>
+                                <p class="change_link">
+                                    <a href="{{url('/')}}"> Back to main page </a>
+                                </p>
+                                <div class="clearfix"></div>
+                                <br />
+                                <div>
+                                    <p>©2023 IVKHK</p>
+                                </div>
+                            </div>
+                        </form>
+                    </section>
+                </div>
+            </div>
         </div>
     </div>
-</div>
+</body>
+</main>
+
 @endsection
--- resources/views/users/register.blade.php
+++ resources/views/users/register.blade.php
@@ -2,46 +2,50 @@
 
 @section('content')
 <hr>
-
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-3">
-            <section class="login_content text-center">
-                @include('common.errors')
-                <form action="{{url('register')}}" method="POST">
-                    {{ csrf_field() }}
-                    <div class="login-form-header">
-                        <h1>Create account</h1>
-                    </div>
-                    <div class="form-group">
-                        <input type="text" class="form-control" name="name" placeholder="Username" required="" />
-                    </div>
-                    <div class="form-group">
-                        <input type="text" class="form-control" name="email" placeholder="Email" required="" />
-                    </div>
-                    <div class="form-group">
-                        <input type="password" class="form-control" name="password" placeholder="Password" required="" />
-                    </div>
-                    <div class="form-group">
-                        <input type="password" class="form-control" name="password_confirmation" placeholder="Password confirmation" required="" />
-                    </div>
-                    <div class="form-group">
-                        <button type="submit" class="btn btn-default submit" name="login">Register</button>
-                    </div>
-                    <p class="change_link">
-                            <a href="{{url('/')}}"> Back to main page </a>
-                        </p>
-                    <div class="clearfix"></div>
-                    <div class="separator">
-                        <div class="clearfix"></div>
-                        <br />
-                        <div>
-                            <p>©2023 IVKHK</p>
-                        </div>
-                    </div>
-                </form>
-            </section>
+<body style="background-color:#242423;">
+    <div class="login-register">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-3">
+                    <section class="login_content text-center">
+                        @include('common.errors')
+                        <form action="{{url('register')}}" method="POST">
+                            {{ csrf_field() }}
+                            <div class="login-form-header">
+                                <h1>Create account</h1>
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control" name="name" placeholder="Username" required="" />
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control" name="email" placeholder="Email" required="" />
+                            </div>
+                            <div class="form-group">
+                                <input type="password" class="form-control" name="password" placeholder="Password" required="" />
+                            </div>
+                            <div class="form-group">
+                                <input type="password" class="form-control" name="password_confirmation" placeholder="Password confirmation" required="" />
+                            </div>
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-default submit" name="login">Register</button>
+                            </div>
+                            <p class="change_link">
+                                    <a href="{{url('/')}}"> Back to main page </a>
+                                </p>
+                            <div class="clearfix"></div>
+                            <div class="separator">
+                                <div class="clearfix"></div>
+                                <br />
+                                <div>
+                                    <p>©2023 IVKHK</p>
+                                </div>
+                            </div>
+                        </form>
+                    </section>
+                </div>
+            </div>
         </div>
     </div>
-</div>
+</body>
+
 @endsection
