970be28c884995793050932f21556059d89afe52
Beshishh <70581429+Beshishh@users.noreply.github.com>
2023-05-14 15:45:46 +0300

    test

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,7 +10,7 @@ use Auth;
 
 class UserController extends Controller
 {
-    public function indexteset()
+    public function indext()
     {
         $role = array('admin', 'client', 'manager');
         $users = User::orderBy('id', 'asc')->get();
