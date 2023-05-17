d6f13cf38922b6ed7af85d095d7e42cfa91540b1
wisayy <davidguzhavin@gmail.com>
2023-05-18 00:55:35 +0300

    BMI Calculator

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -101,33 +101,69 @@
                                                     @endif
                                                 </div>
                                             </div>
-                                            /* BMI */
-                                            <h6 class="m-b-20 m-t-40 p-b-5 b-b-default text-center f-w-600">BMI</h6>
-                                            <div class="row text-center">
-                                                <h1>BMI Calculator</h1>
-
-                                                @if (session('bmi'))
-                                                    <h2>Your BMI: {{ session('bmi') }}</h2>
-                                                    <h3>Category: {{ session('bmiCategory') }}</h3>
-                                                @endif
-
-                                                <h2 class="bmi-result"></h2>
-                                                <h3 class="bmi-category-result"></h3>
-
-                                                <form id="bmi-calculator-form">
-                                                    <label for="weight">Weight (kg):</label>
-                                                    <input type="text" name="weight" id="weight" required><br>
-
-                                                    <label for="height">Height (m):</label>
-                                                    <input type="text" name="height" id="height" required><br>
-
-                                                    <button type="submit">Calculate</button>
-                                                </form>
-                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
+                            <div class="card user-card-full bmiForm">
+                               <div class="row m-l-0 m-r-0">
+                                   <div class="col-sm-8 w-100">
+                                       <div class="card-block">
+                                           <div class="row text-center justify-content-center">
+
+                                            <h1>BMI Calculator</h1>
+                                             <button type="button" class="btn btn-info align-center" data-container="body" data-toggle="popover" data-placement="bottom" data-content="Body Mass Index (BMI) is a numerical value that is calculated based on a person's weight and height. It is commonly used as an indicator of body fatness and can provide a general assessment of whether a person has a healthy body weight in relation to their height.">
+                                             <i class="fa-sharp fa-regular fa-question fa-xl"></i>
+                                             </button>
+
+                                            <form class="form-group col-md-3" id="bmiForm">
+                                                <label for="height">Height (cm):</label>
+                                                    <input type="text" name="height" id="height" class="form-control text-center" required><br>
+                                                <label for="weight">Weight (kg):</label>
+                                                    <input type="text" name="weight" id="weight" class="form-control text-center" required><br>
+                                                <button type="submit" class="btn btn-default submit ">Calculate </button>
+                                            </form>
+
+                                            <div id="result"></div>
+
+                                            <script>
+                                                document.getElementById('bmiForm').addEventListener('submit', function(event) {
+                                                    event.preventDefault(); // Отменить стандартное поведение формы
+
+                                                    var weight = parseFloat(document.getElementById('weight').value);
+                                                    var height = parseFloat(document.getElementById('height').value);
+
+                                                    // Проверить, что введены числовые значения для веса и роста
+                                                    if (isNaN(weight) || isNaN(height)) {
+                                                        alert('Please enter valid numeric values for weight and height.');
+                                                        return;
+                                                    }
+
+                                                    // Рассчитать ИМТ
+                                                       var bmi = weight / ((height/100)*(height/100));
+
+                                                    // Определить категорию ИМТ
+                                                    var bmiCategory = '';
+                                                    if (bmi < 18.5) {
+                                                        bmiCategory = 'Underweight';
+                                                    } else if (bmi >= 18.5 && bmi <= 24.9) {
+                                                        bmiCategory = 'Normal weight';
+                                                    } else if (bmi >= 25 && bmi <= 29.9) {
+                                                        bmiCategory = 'Overweight';
+                                                    } else if (bmi >= 30) {
+                                                        bmiCategory = 'Obesity';
+                                                    }
+
+                                                    // Отобразить результаты
+                                                    var resultElement = document.getElementById('result');
+                                                    resultElement.innerHTML = '<h3 class="m-0">Your BMI: ' + bmi.toFixed(2) + '</h3><h3 class="m-0">Category: ' + bmiCategory + '</h3>';
+                                                });
+                                            </script>
+                                           </div>
+                                       </div>
+                                   </div>
+                               </div>
+                           </div>
                         </div>
                     </div>
                 </div>
@@ -135,36 +171,4 @@
         </section>
     </main>
 
-    <script>
-        const form = document.getElementById('bmi-calculator-form');
-        const bmiResult = document.querySelector('.bmi-result');
-        const bmiCategoryResult = document.querySelector('.bmi-category-result');
-
-        const handleSubmit = (event) => {
-            event.preventDefault();
-
-            const weight = event.target.elements.weight.value;
-            const height = event.target.elements.height.value;
-
-            const bmi = (weight / (height * height)).toFixed(2);
-            let bmiCategory = '';
-
-            if (bmi < 18.5) {
-                bmiCategory = 'Underweight';
-            } else if (bmi >= 18.5 && bmi <= 24.9) {
-                bmiCategory = 'Normal weight';
-            } else if (bmi >= 25 && bmi <= 29.9) {
-                bmiCategory = 'Overweight';
-            } else if (bmi >= 30) {
-                bmiCategory = 'Obesity';
-            }
-
-            bmiResult.innerHTML = `Your BMI: ${bmi}`;
-            bmiCategoryResult.innerHTML = `Category: ${bmiCategory}`;
-
-            form.reset();
-        }
-
-        form.addEventListener('submit', handleSubmit);
-    </script>
 @endsection
