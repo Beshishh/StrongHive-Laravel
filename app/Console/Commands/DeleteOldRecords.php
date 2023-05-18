b193c7b104e91c7a417bce99326a2f85333ac712
Beshishh <70581429+Beshishh@users.noreply.github.com>
2023-05-18 13:07:28 +0300

    Fix

--- app/Console/Commands/DeleteOldRecords.php
+++ app/Console/Commands/DeleteOldRecords.php
@@ -3,7 +3,6 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
-use SimpleSoftwareIO\QrCode\Facades\QrCode;
 use App\Models\Orders;
 
 class DeleteOldRecords extends Command
@@ -11,13 +10,8 @@ class DeleteOldRecords extends Command
     protected $signature = 'delete:old-records';
     protected $description = 'Delete records older than 30 days';
 
-    public function handle(Orders $orders)
+    public function handle()
     {
-        $qrCodePath = public_path('qr-codes/' . $orders->qr);
-
-        if (file_exists($qrCodePath)) {
-            unlink($qrCodePath);
-        }
         Orders::where('subEnd', '<', now())->delete();
         $this->info('Old records have been deleted successfully.');
     }
